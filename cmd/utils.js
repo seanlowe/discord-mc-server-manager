4386f209fbf49b0dbedb89a8f3e41ea8c31da296
Sean Lowe <35589586+seanlowe@users.noreply.github.com>
2026-01-03 21:34:19 -0700

    see if this works in prod

--- cmd/utils.js
+++ cmd/utils.js
@@ -2,6 +2,34 @@ import { execa } from 'execa'
 import { access } from "node:fs/promises"
 import { constants, writeFileSync, readFileSync } from "node:fs"
 
+// use a command builder instead of string shenanigans
+// to determine what gets passed to execa
+// when using, do a [command ...args] to get startingCommand and commandsArray
+const dockerCommands = {
+  psAllJson: () => {
+    return ["docker", "ps", "-a", "--format", "{{json .}}"]
+  },
+  start: (serverName) => {
+    return ["docker", "start", serverName]
+  },
+  stop: (serverName) => {
+    return ["docker", "stop", serverName]
+  },
+  restart: (serverName) => {
+    return ["docker", "restart", serverName]
+  },
+  execRcon: (serverName, rconArgs) => {
+    return ["docker", "exec", serverName, "rcon-cli", "--", ...rconArgs]
+  },
+  logsGrep: (sinceTime, serverName) => {
+    // `docker logs --since ${sinceTime} ${serverName} | grep -F "[minecraft/DedicatedServer]: Done"`
+    return ["docker", "logs", "--since", sinceTime, serverName, "|", "grep", "-F", "[minecraft/DedicatedServer]: Done"]
+  }
+}
+
+// example usage:
+// dockerCommands.execRcon("mc-survival", ["list"])
+
 const allowedMinecraftServers = JSON.parse(
   readFileSync(
     new URL("../data/allowed-minecraft-servers.json", import.meta.url),
@@ -28,7 +56,8 @@ export const convertMsToISO = (ms) => {
 }
 
 export const getDockerContainers = async () => {
-  const result = await makeDockerCall("docker ps -a --format '{{json .}}'")
+  // const result = await makeDockerCall("docker ps -a --format '{{json .}}'")
+  const result = await makeDockerCall(dockerCommands.psAllJson())
 
   const rawContainerOutput = result.split('\n').map(line => JSON.parse(line))
     .filter(container => allowedMinecraftServerNames.includes(container.Names))
@@ -42,7 +71,9 @@ export const getDockerContainers = async () => {
   }))
 }
 
-export const makeDockerCall = async (command) => {
+export const makeDockerCall = async (commandsArray, shouldUseShell = false) => {
+  // commandsArray is an array of shit that looks like this:
+  // ["docker", "start", "vanilla-minecraft"]
   const {
     NODE_ENV,
     SSH_USER,
@@ -54,45 +85,70 @@ export const makeDockerCall = async (command) => {
   // then construct the execa command using ssh and the original command passed through,
   // if we are deployed on the server (i.e. "prod"), then construct the execa command using the original command passed through
 
-  let commandsArray = []
+  // let commandsArray = []
   let startingCommand = null
   let commandEnv = {}
-  if (NODE_ENV === 'production') {
-    // use docker whatever
+  if (shouldUseShell) {
+    commandEnv = {
+      shell: true,
+    }
+  }
 
-    // "docker start vanilla-minecraft" becomes ["docker", "start", "vanilla-minecraft"]
-    commandsArray = command.split(' ')
+  switch (NODE_ENV) {
+    case 'production': {
+      console.log('running production logic')
+      if (shouldUseShell) {
+        // (we're running the container recheck loop)
+        // we need shell features in order to use the `|` pipe operator
+        // our command should be all one line
+        startingCommand = commandsArray.join(' ')
+        break
+      }
 
-    // should just be "docker" -> leaving commandsArray as ["start", "vanilla-minecraft"]
-    startingCommand = commandsArray.shift()
-  } else if (NODE_ENV === "development") {
-    // need this so I can test it locally / get info from the server while developing locally
-    const askpass = "/tmp/askpass.sh"
-    if (!fileExists(askpass)) {
-      writeFileSync(
-        askpass,
-        `#!/bin/sh\necho "$SSH_PASSWORD"\n`,
-        { mode: 0o700 }
-      )
+      startingCommand = commandsArray.shift()
+      break
     }
 
-    // use ssh
-    startingCommand = 'ssh'
-    commandsArray = [
-      `${SSH_USER}@${SSH_HOST}`,
-      command
-    ]
-    commandEnv = {
-      env: {
-        ...process.env,
-        SSH_ASKPASS: askpass,
-        SSH_PASSWORD,
-        SSH_ASKPASS_REQUIRE: "prefer",
-      },
+    case "development": {
+      // need this so I can test it locally / get info from the server while developing locally
+      const askpass = "/tmp/askpass.sh"
+      if (!fileExists(askpass)) {
+        writeFileSync(
+          askpass,
+          `#!/bin/sh\necho "$SSH_PASSWORD"\n`,
+          { mode: 0o700 }
+        )
+      }
+  
+      // use ssh
+      // startingCommand = 'ssh'
+      // commandsArray = [
+      //   `${SSH_USER}@${SSH_HOST}`,
+      //   command
+      // ]
+  
+      // add ssh and `${SSH_USER}@${SSH_HOST}` to the commandsArray
+      startingCommand = `ssh`
+      commandsArray = [`${SSH_USER}@${SSH_HOST}`, commandsArray.join(' ')]
+  
+      commandEnv = {
+        ...commandEnv,
+        env: {
+          ...process.env,
+          SSH_ASKPASS: askpass,
+          SSH_PASSWORD,
+          SSH_ASKPASS_REQUIRE: "prefer",
+        },
+      }
     }
   }
 
   try {
+    if (shouldUseShell) {
+      const result = await execa(startingCommand, commandEnv)
+      return result.stdout.trim()
+    }
+
     const result = await execa(startingCommand, commandsArray, commandEnv)
     return result.stdout.trim()
   } catch (e) {
@@ -109,8 +165,7 @@ export const containerRecheckLoop = async (interaction, serverName, startTime, s
 
   // wait and confirm the server is running
   while (Date.now() - startTime <= MAX_WAIT_TIME) {
-    const cmd = `docker logs --since ${sinceTime} ${serverName} | grep -F "[minecraft/DedicatedServer]: Done"`
-    const result = await makeDockerCall(cmd)
+    const result = await makeDockerCall(dockerCommands.logsGrep(sinceTime, serverName), true)
 
     if (!result) {
       console.log(`Not ready yet. . . waiting ${RECHECK_INTERVAL / 1000} seconds.`)

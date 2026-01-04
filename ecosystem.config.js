73d03636ca139df88cf98fc4d1bffea9e4fd3bbf
Sean Lowe <35589586+seanlowe@users.noreply.github.com>
2026-01-03 17:48:57 -0700

    update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,29 +1,29 @@
-export const apps = [{
-  name: "discord-mc-server-manager",
-  script: "./discord-mc-server-manager.js",
-  env: {
-    NODE_ENV: "development"
-  },
-  env_production: {
-    NODE_ENV: "production",
-  }
-}]
+// export const apps = [{
+//   name: "discord-mc-server-manager",
+//   script: "./discord-mc-server-manager.js",
+//   env: {
+//     NODE_ENV: "development"
+//   },
+//   env_production: {
+//     NODE_ENV: "production",
+//   }
+// }]
 
-// module.exports = {
-//   apps: [{
-//     name: "app",
-//     script: "./app.js",
-//     env: {
-//       NODE_ENV: "development"
-//     },
-//     env_test: {
-//       NODE_ENV: "test",
-//     },
-//     env_staging: {
-//       NODE_ENV: "staging",
-//     },
-//     env_production: {
-//       NODE_ENV: "production",
-//     }
-//   }]
-// }
+module.exports = {
+  apps: [{
+    name: "discord-mc-server-manager",
+    script: "./discord-mc-server-manager.js",
+    env: {
+      NODE_ENV: "development"
+    },
+    // env_test: {
+    //   NODE_ENV: "test",
+    // },
+    // env_staging: {
+    //   NODE_ENV: "staging",
+    // },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }]
+}
